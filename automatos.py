32bfa7ecab8505bc1ae3e4373705f6bd9648b3e2
Genesio Valois Coutinho Neto <valois23@(none)>
2012-12-14 14:51:08 -0300

    automato string

--- automatos.py
+++ automatos.py
@@ -25,15 +25,18 @@ estados[1].set_transicoes({"\r\n": estados[2],"outro":estados[1]})
 automatos["comentario"] = Automato(estados, estados[0])
 
 
-#----------------------- STRING
-
+#-------------------------- STRING
 estados = []
 estados.append(Estado("q0", False))
 estados.append(Estado("q1", False))
-estados.append(Estado("q2", True))
+estados.append(Estado("q2", False))
+estados.append(Estado("q3", True))
+estados.append(Estado("erro", False))
 
-estados[0].set_transicoes({"#": estados[1]})
-estados[1].set_transicoes({"\r\n": estados[2],"outro":estados[1]})
+estados[0].set_transicoes({"'": estados[1]},'"':estados[2])
+estados[1].set_transicoes({"'": estados[3],"outro":estados[1],"\r\n": estados[4]})
+estados[2].set_transicoes({'"': estados[3],"outro":estados[2],"\r\n": estados[4]})
+
 
 automatos["string"] = Automato(estados, estados[0])
 
