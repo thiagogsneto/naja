974bcd12c4dde111b884acc9caea1810a9da4b35
thiagogsneto <thiagogsneto@gmail.com>
2012-12-15 16:36:33 -0200

    Update automatos.py
    Adicionei os autômatos para operadores lógicos e matemáticos

--- automatos.py
+++ automatos.py
@@ -40,6 +40,69 @@ estados[2].set_transicoes({'"': estados[3],"outro":estados[2],"\n": estados[4]})
 automatos["string"] = Automato("string", estados, estados[0])
 
 
+#--------------------------- OP LOGICO
+estados = []
+estados.append(Estado("q0", False))
+estados.append(Estado("q1", False))
+estados.append(Estado("q4", False))
+estados.append(Estado("q7", False))
+estados.append(Estado("q10", False))
+estados.append(Estado("q13", False))
+estados.append(Estado("q15", False))
+estados.append(Estado("q2", True))
+estados.append(Estado("q3", True))
+estados.append(Estado("q5", True))
+estados.append(Estado("q6", True))
+estados.append(Estado("q8", True))
+estados.append(Estado("q9", True))
+estados.append(Estado("q11", True))
+estados.append(Estado("q12", True))
+estados.append(Estado("q14", True))
+estados.append(Estado("q16", True))
+
+estados[0].set_transicoes({"=": estados[1], "<": estados[4], ">": estados[7], "!": estados[10], "&": estados[13], "|": estados[15]})
+estados[1].set_transicoes({"=": estados[2], "outro": estados[3]})
+estados[4].set_transicoes({"=": estados[5], "outro": estados[6]})
+estados[7].set_transicoes({"=": estados[8], "outro": estados[9]})
+estados[10].set_transicoes({"=": estados[11], "outro": estados[12]})
+estados[13].set_transicoes({"&": estados[14]})
+estados[15].set_transicoes({"|": estados[16]})
+
+automatos["oplogico"] = Automato("oplogico"), estados, estados[0])
+
+
+#--------------------------- OP MATEMATICO
+estados = []
+estados.append(Estado("q1", False))
+estados.append(Estado("q4", False))
+estados.append(Estado("q7", False))
+estados.append(Estado("q8", False))
+estados.append(Estado("q13", False))
+estados.append(Estado("q15", False))
+estados.append(Estado("q2", True))
+estados.append(Estado("q3", True))
+estados.append(Estado("q5", True))
+estados.append(Estado("q6", True))
+estados.append(Estado("q9", True))
+estados.append(Estado("q10", True))
+estados.append(Estado("q11", True))
+estados.append(Estado("q12", True))
+estados.append(Estado("q14", True))
+estados.append(Estado("q16", True))
+estados.append(Estado("q17", True))
+estados.append(Estado("q18", True))
+
+estados[0].set_transicoes({"+": estados[1], "-": estados[4], "*": estados[7], "/": estados[13]})
+estados[1].set_transicoes({"=": estados[2], "outro": estados[3]})
+estados[4].set_transicoes({"=": estados[5], "outro": estados[6]})
+estados[7].set_transicoes({"*": estados[8], "=": estados[11], "outro": estados[12]})
+estados[8].set_transicoes({"=": estados[9], "outro": estados[10]})
+estados[13].set_transicoes({"|": estados[15], "=": estados[16], "outro": estados[14]})
+estados[15].set_transicoes({"=": estados[17], "outro": estados[18]})
+
+automatos["opmatematico"] = Automato("opmatematico"), estados, estados[0]
+
+
 
 
 
